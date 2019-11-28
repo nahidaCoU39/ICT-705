88c0c75a11970d1414e05ddc6aed323274e15ac9
nahidaCoU39 <50311828+nahidaCoU39@users.noreply.github.com>
2019-11-28 04:46:44 -0800

    Add files via upload

---
+++ register/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>user Login</title>
+</head>
+<body>
+	<form action="login_action.php" method="post">
+        
+		
+		UserName:<br>
+		<input type="text" name="username" placeholder="Enter username"/><br><br>
+		Password:<br>
+		<input type="text" name="password" placeholder="Enter password"/><br><br>
+		
+		<input type="submit" name="login" value="LOGIN" /><br>
+
+		
+
+	</form>
+
+</body>
+</html>
---
+++ register/registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>user registration</title>
+</head>
+<body>
+	<form action="action_reg.php" method="post">
+        
+		Name:<br>
+		<input type="text" name="name" placeholder="Enter name"/><br><br>
+		UserName:<br>
+		<input type="text" name="username" placeholder="Enter username"/><br><br>
+		Password:<br>
+		<input type="text" name="password" placeholder="Enter password"/><br><br>
+		E-mail:<br>
+		<input type="text" name="email" placeholder="Enter email"/><br><br><br>
+		<input type="submit" name="submit" value="Register" /><br>
+
+		
+
+	</form>
+
+</body>
+</html>
